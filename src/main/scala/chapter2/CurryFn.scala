cc2dc38c1722cd10383534b4e142810eb9b1228c
manuel <manolo85gn@gmail.com>
2015-01-28 20:48:42 -0600

    implemented curry exercise

---
+++ src/main/scala/chapter2/CurryFn.scala
@@ -0,0 +1,11 @@
+package chapter2
+
+object CurryFn {
+  def curry[A,B,C](f: (A, B) => C ) : A => (B => C) = {
+    (a: A) => (b: B) => f(a, b)
+  } 
+
+  def uncurry[A, B, C](f:A => B => C) : (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+}
