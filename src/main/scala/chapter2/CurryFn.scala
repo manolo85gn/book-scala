c434bb42c6c17408308d2b21e17b5c4b52a6db9b
manuel <manolo85gn@gmail.com>
2015-02-03 19:35:34 -0600

    Added curry tests

--- src/main/scala/chapter2/CurryFn.scala
+++ src/main/scala/chapter2/CurryFn.scala
@@ -8,4 +8,9 @@ object CurryFn {
   def uncurry[A, B, C](f:A => B => C) : (A, B) => C = {
     (a: A, b: B) => f(a)(b)
   }
+/*
+  def compose[A, B, C](f: B => C, g: A => B) : A => C = {
+    
+  }*/
+
 }
